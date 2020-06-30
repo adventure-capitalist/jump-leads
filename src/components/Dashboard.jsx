df3c492bc8a36c49d8e5ca85b41ea37f994cc8b6
adventure-capitalist <ashleylaurel@outlook.com>
2020-06-30 17:43:38 +0100

    importing to the dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,6 +6,9 @@ import Lesson8 from "./Lesson8";
 import Lesson9 from "./Lesson9";
 import Lesson10 from "./Lesson10";
 import Lesson11 from "./Lesson11";
+import Lesson12 from "./Lesson12";
+import Lesson13 from "./Lesson13";
+import Lesson14 from "./Lesson14";
 
 class Dashboard extends Component {
     state = { 
