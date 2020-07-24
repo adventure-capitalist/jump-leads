91df13b08bf816d5ba61aa9a425651d1e7c0e315
adventure-capitalist <ashleylaurel@outlook.com>
2020-07-24 21:49:50 +0100

    build

---
+++ src/components/Challenge23.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import {GoDashboard, GoBook} from "react-icons/go";
+import Lesson23 from './Lesson23';
+
+class Challenge23 extends Component {
+
+    state = {  }
+
+    render() { 
+        return ( 
+        <div className="mainHolder">
+            <div className="lessonHeader">
+                <h2 className="lessonTitle">Lesson 23: Operators</h2>
+                <div class="icons">
+                    <GoBook onClick={() => this.props.callback(<Lesson23 callback={this.props.callback}/>)} className="dashIcon"/>
+                    <GoDashboard onClick={() => this.props.callback(null)} className="dashIcon"/>
+                </div>
+            </div>
+                <div className="spacer"></div>
+                    <div className="lessonBody">
+                        <p>Fork the Code Pen bellow follow the instructions written in the JavaScript column.</p>
+                        <p>Here you will see several different challenges based on arrays. Best of luck and have fun!</p>
+                        <p className="codepen" data-height="309" data-theme-id="light" data-default-tab="css,result" data-user="ashleylaurel" data-slug-hash="rNxVzxY" style={{height: "309px", boxSizing: "border-box", display: "flex", alignItems: "center", justifyContent: "center", border: "2px solid", margin: "1em 0", padding: "1em"}} data-pen-title="Array of Sunshine">
+                            <span>See the Pen <a href="https://codepen.io/ashleylaurel/pen/XWXOyeq">Array of Sunshine</a></span>
+                        </p>
+                    </div>
+        
+        </div> 
+        );
+    }
+}
+ 
+export default Challenge23;
