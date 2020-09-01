67962bfee4ca18e919fd800bb8ce4a48457789bf
Ashley Laurel Mikkola <ashleylaurel@outlook.com>
2020-09-01 15:58:07 +0100

    adding in the new memory game

--- src/components/Challenge34.jsx
+++ src/components/Challenge34.jsx
@@ -10,7 +10,7 @@ class Challenge34 extends Component {
         return ( 
         <div className="mainHolder">
             <div className="lessonHeader">
-                <h2 className="lessonTitle">Lesson 34: JavaScript DOM</h2>
+                <h2 className="lessonTitle">Lesson 34: JavaScript Transport</h2>
                 <div class="icons">
                     <GoBook onClick={() => this.props.callback(<Lesson34 callback={this.props.callback}/>)} className="dashIcon"/>
                     <GoDashboard onClick={() => this.props.callback(null)} className="dashIcon"/>
@@ -18,11 +18,7 @@ class Challenge34 extends Component {
             </div>
                 <div className="spacer"></div>
                     <div className="lessonBody">
-                        <p>Fork the Code Pen bellow follow the instructions written in the JavaScript column.</p>
-                        <p>Here you will see a challenge for you based off of APIs and JSON. Best of luck and have fun!</p>
-                        <p className="codepen" data-height="309" data-theme-id="light" data-default-tab="css,result" data-user="ashleylaurel" data-slug-hash="rNxVzxY" style={{height: "309px", boxSizing: "border-box", display: "flex", alignItems: "center", justifyContent: "center", border: "2px solid", margin: "1em 0", padding: "1em"}} data-pen-title="It's raining cats and dogs">
-                            <span>See the Pen <a href="https://codepen.io/ashleylaurel/pen/Yzqweer">It's raining cats and dogs</a></span>
-                        </p>
+                        <p>Play this HTTP Error code level matching game and learn what the different levels mean: <a href="https://adventure-capitalist.github.io/HTTP-Status-Code-Memory-Game/">HTTP Error Memory Game</a></p>
                     </div>
         
         </div> 
